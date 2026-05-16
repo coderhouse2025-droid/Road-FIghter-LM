9b363f53b00d19e7093bb580b6ff37e8591b4c63
JUANMA <coderhouse2025@gmail.com>
2026-05-16 10:23:30 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'rf-v2';
+const FILES = ['./', './index.html', './style.css', './js/audio.js', './js/sprites.js', './js/game.js', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
